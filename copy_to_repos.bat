9595015ce51f2576c6c9f12535b1940cdcd8b70c
Michael Munz <michael.munz@thu.de>
2026-08-12 15:16:40 +0200

    fix copy_to_repos

--- copy_to_repos.bat
+++ copy_to_repos.bat
@@ -1,5 +1,5 @@
 call C:\Users\mmunz\Anaconda3\Scripts\activate.bat 
 call C:\Users\micha\Anaconda3\Scripts\activate.bat 
 call C:\ProgramData\Anaconda3\Scripts\activate.bat 
-python deploy.py
+python copy_to_repos.py
 pause
